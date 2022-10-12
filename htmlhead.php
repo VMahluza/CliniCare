c3983daaba40967c69448ad74a5bf02fefb3f2e5
Perfect Dev <perfect.coding98@gmail.com>
2022-10-12 13:01:02 +0200

    Update on html head - removed bootstrap CND

--- htmlhead.php
+++ htmlhead.php
@@ -1,8 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <meta name="description" content="CliniCare modern health care services for all">
     <title>Clinicare | <?php echo $title;?></title>
+    <link rel="stylesheet" href="../public/styles.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
+<body>
+    <header class="header">
+        <nav class="header__navbar container">
+            <div class="header__logo">
+                <img src="../public/images/logo.svg" alt="">
+            </div>
+            <ul class="navbar__list">
+                <li><a href="#">Patients</a></li>
+                <li><a href="#">Add New Patient</a></li>
+            </ul>
+
+            <a href="#">Contact IT support</a>
+        </nav>
+    </header>
+
