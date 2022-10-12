7b29b03e58048944e5df76df527a112f5cef5033
Perfect Dev <perfect.coding98@gmail.com>
2022-10-12 12:44:13 +0200

    Added public folder

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+const password =  document.getElementById('password')
+const reapetPassword =  document.getElementById('repeat_password')
+const showPasswordBtns = document.querySelectorAll('.password-show')
+
+
+//######################### SHOWING AND HIDDING PASSWORD ############################################
+showPasswordBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const clickedBtnId = e.target.id
+        toggleShowPassword(clickedBtnId)
+    })
+})
+
+
+function toggleShowPassword(clickedBtnId) {
+    let passwordInput
+    if (clickedBtnId === 'show-password') {
+        passwordInput = document.getElementById('password')
+    }
+    if (clickedBtnId === 'show-repeat-password') {
+        passwordInput = document.getElementById('repeat-password')
+    }
+
+    if (passwordInput.getAttribute('type') === 'password') {
+        passwordInput.setAttribute('type', 'text')
+    }
+    else {
+        passwordInput.setAttribute('type', 'password')
+    }
+}
