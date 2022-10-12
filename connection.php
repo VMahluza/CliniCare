0718bb557ff39ed67d84d56e44e3b4fd6e546ca7
Perfect Dev <perfect.coding98@gmail.com>
2022-10-12 12:38:41 +0200

    Fixed indentation on connection.php

--- connection.php
+++ connection.php
@@ -13,8 +13,8 @@ $db = null;
 try {
 
     $db = new PDO("mysql:host={$db_host};dbname={$db_name}",
-                                $db_user,
-                                $db_password);
+    $db_user,
+    $db_password);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
